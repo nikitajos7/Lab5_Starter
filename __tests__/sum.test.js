11786c3f8a47d9f5a7181bf3ae26ff7f73abecc0
nikita-jos <nijos@ucsd.edu>
2026-05-05 19:03:03 -0700

    Fix failing test

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -3,7 +3,7 @@
 import { sum } from '../code-to-unit-test/sum.js';
 
 test('adds 1 + 2 to equal 3 without sum function', () => {
-  expect(1 + 2).toBe(4);
+  expect(1 + 2).toBe(3);
 });
 
 test('adds 1 + 2 to equal 3 with sum function', () => {
